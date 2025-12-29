d8d0c2b850e05ebf4ad3f297d5977cc9f816933c
StefanoZanardo <stefano.zanardo.24@stud.itsaltoadriatico.it>
2025-12-29 21:34:45 +0100

    Secondo miglioramento del 29/12/2025

--- simulatore-stazione-treno/RailSegment.gd
+++ simulatore-stazione-treno/RailSegment.gd
@@ -1,19 +1,28 @@
 extends Node2D
 class_name RailSegment
 
-func get_global_points() -> PackedVector2Array:
-	var total_points = PackedVector2Array()
+
+func get_global_points() -> BinarioInfo:
+	var info_rail = BinarioInfo.new()
 	
 	# Scorre tutti i nodi figli di questo RailSegment
 	for child in get_children():
 		
 		# Controlla se il figlio è una Line2D
-		if child is Line2D:
+		if child is Line2D && child.name.to_lower().begins_with('bivio'):
+			
+			var linea = child
+			var xform = linea.get_global_transform()
+			
+
+			for p in linea.points:
+				info_rail.crossroad.append(xform * p)
+		elif child is Line2D && child.name.to_lower().begins_with('line'):
 			var linea = child
 			var xform = linea.get_global_transform()
 			
 			# Converte i punti di QUESTA linea in globali e li aggiunge al totale
 			for p in linea.points:
-				total_points.append(xform * p)
+				info_rail.rail_segment.append(xform * p)
 	
-	return total_points
+	return info_rail
--- simulatore-stazione-treno/Treno.gd
+++ simulatore-stazione-treno/Treno.gd
@@ -5,25 +5,41 @@ extends Node2D
 var line_node : Line2D
 var rail_point : PackedVector2Array
 var current_point_index : int = 0
-var crossroad : PackedVector2Array 
+var crossroadPoints : PackedVector2Array 
+var railPoints : BinarioInfo
 var activepath : PackedVector2Array
-var isCrossRoad : bool
-var c : int = 0
+var ChangeLaneRail : bool
+var ActualCrossRoad : PackedVector2Array
+var foward : bool
 
 
 
 func _ready():
-	if rail_ != null:
-		print(rail_.get_global_points())
-	rail_point = rail_.get_global_points()
-	print(rail_point)
+
+		
+	railPoints = rail_.get_global_points()
+	
+	crossroadPoints = railPoints.crossroad
+	rail_point = railPoints.rail_segment
+	#Questo è hard coded poi dovrà essre messo dinamicamente 
+	ChangeLaneRail = true
+	#Anche questo è hardcoded
+	foward = true
+	
 	
 
 	
 	global_position = rail_point[0]
 	current_point_index = 1
 	activepath = rail_point
-	isCrossRoad = false
+	#Qua quello che facciamo è dire al nostro treno qual è la nostra crossroad attuale
+	ActualCrossRoad = _actual_crossroadpoint(crossroadPoints)
+	
+	print(ActualCrossRoad)
+	
+	
+	
+
 	
 	
 	
@@ -35,29 +51,47 @@ func _physics_process(delta: float) -> void:
 		var direction: Vector2 = (target_point - global_position).normalized()
 		var distance_to_travel : float = delta * speed
 		var distance_to_target_point : float = global_position.distance_to(target_point)
+		if ChangeLaneRail == true && global_position.distance_to(ActualCrossRoad[0]) < 20.0:
+			activepath[0] = ActualCrossRoad[0]
+			activepath[1] = ActualCrossRoad[1]
+			ActualCrossRoad = _actual_crossroadpoint(crossroadPoints)
 		if distance_to_travel >= distance_to_target_point:
 			_filter_points()
 			print(activepath)
 			global_position = target_point
-			#current_point_index += 1
 			if current_point_index >= activepath.size():
+				print(activepath)
 				set_physics_process(false)
+		
+		
+			
 		else:
 			print(direction)
 			print(distance_to_travel)
 			global_position += direction*distance_to_travel
 			rotation = direction.angle()
 		
-func _switch_process() -> bool:
-	return abs(global_position.y - crossroad[0].y) < 2.0	
 	
+#Funzione che toglie tutti i punti non neccessari per il treno	(Questa bisognerà rivederla in modo che si 
 func _filter_points() -> void:
 	var new_path = PackedVector2Array()
 	var near : int = 55
 	for punto in activepath:
 		if (punto.y-global_position.y) < 55 and (punto.x-global_position.x) > 10:
 			new_path.append(punto)
-	
-
 	activepath = new_path
 	current_point_index = 0
+#Questa funzione ritorna la crossroad che dovrà fare 
+func _actual_crossroadpoint(list_crossroad : PackedVector2Array) -> PackedVector2Array:
+	var _crossroadP : PackedVector2Array = [Vector2(9999,9999), Vector2(9999,9999)]
+	for i in range (list_crossroad.size()-1):
+		if (list_crossroad[i].x - global_position.x) > 5.0 && abs((list_crossroad[i].y - global_position.y)) < 5.0:
+			if _crossroadP[0] > list_crossroad[i]:
+				if foward == true:
+					_crossroadP=[list_crossroad[i], list_crossroad[i+1]]
+				else:
+					_crossroadP=[list_crossroad[i], list_crossroad[i-1]]
+	return _crossroadP
+	
+#func _addNewPath
+	
